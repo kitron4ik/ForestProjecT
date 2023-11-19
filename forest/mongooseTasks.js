1990379505882a7a8df72a00dad7372e3e21837b
kitron4ik <craftukww@gmail.com>
2023-11-20 00:24:09 +0300

    Создал схему

--- forest/mongooseTasks.js
+++ forest/mongooseTasks.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testik');
 
-const Tree = mongoose.model('Tree', { name: String });
+var schema = mongoose.Schema({name: String})
+
+schema.methods.scholoh = function() {
+    console.log(this.get("name") + " колыхнул своими ветками")
+}
+
+const Tree = mongoose.model('Tree', schema);
 
 const tree = new Tree({ name: 'Дуб' });
-tree.save().then(() => console.log('Шелох листьев'));
+tree.save().then(function(err){
+    tree.scholoh()
+})
