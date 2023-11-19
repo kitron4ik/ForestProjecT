45d773c4060778e9cfad2e54d852e537de8e8db0
kitron4ik <craftukww@gmail.com>
2023-11-20 00:58:42 +0300

    Сделал обработку параметра в адрессе

--- forest/app.js
+++ forest/app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/forest');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var trees = require('./routes/trees');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trees', trees);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
