2f608dfff4bf275bb9c5bd963025e8f4a5177415
kitron4ik <craftukww@gmail.com>
2023-11-19 23:15:54 +0300

    Создал модуль данных

---
+++ forest/data.js
@@ -0,0 +1,19 @@
+var data = [{
+    title: 'Дубина',
+    nick: 'Dubina',
+    avatar: '/images/Dubina.png',
+    desc: 'Белое дерево в черное пятнышко с вкусным березовым соком'
+    },
+    {
+    title: 'Ёлка',
+    nick: 'Elochka',
+    avatar: '/images/Elochka.png',
+    desc: 'Дерево с приятным запахом, вечный гость в новогоднюю ночь!'
+    },
+    {
+    title: 'Берёзка',
+    nick: 'Berezka',
+    avatar: '/images/Berezka.png',
+    desc: 'Древнерусское древо, оно могло застать людей в начале нашей эры, так как может жить до 2000 лет!'
+    }
+    ];
