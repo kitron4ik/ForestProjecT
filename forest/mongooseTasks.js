f512e2bece3bb0785199735b03ff4bff937f00ea
kitron4ik <craftukww@gmail.com>
2023-11-20 00:34:39 +0300

    Создал модель данных приложения

--- forest/mongooseTasks.js
+++ forest/mongooseTasks.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testik');
+mongoose.connect('mongodb://127.0.0.1:27017/testik1');
+var Tree = require("./models/tree").Tree
 
 var schema = mongoose.Schema({name: String})
 
-schema.methods.scholoh = function() {
-    console.log(this.get("name") + " колыхнул своими ветками")
-}
-
-const Tree = mongoose.model('Tree', schema);
-
-const tree = new Tree({ name: 'Дуб' });
-tree.save().then(function(err){
-    tree.scholoh()
+var tree = new Tree ({
+    title:"Дубинский",
+    nick:"Dubinskii"
+})
+console.log(tree)
+tree.save().then(function(err, tree, affected){
+    console.log(tree.title)
 })
