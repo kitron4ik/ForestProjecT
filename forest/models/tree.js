ea1b9d6e454cbadd0edc9bc4e63ecb202ff375f1
kitron4ik <craftukww@gmail.com>
2023-11-20 21:22:33 +0300

    Данных из базы

--- forest/models/tree.js
+++ forest/models/tree.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 var treeSchema = new Schema({
-title: String,
-nick: {
-type: String,
-unique: true,
-required: true
-},
-avatar: String,
-desc: String,
-created:{
-type:Date,
-default:Date.now
-}
+    title: String,
+    nick: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    avatar: String,
+    desc: String,
+    created:{
+        type:Date,
+        default:Date.now
+    }
 })
-module.exports.Tree = mongoose.model("Tree", treeSchema)
+module.exports.Tree = mongoose.model("tree", treeSchema)
--- forest/routes/trees.js
+++ forest/routes/trees.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var Tree = require("../models/tree").Tree
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с tree');
@@ -11,5 +13,16 @@ router.get('/', function(req, res, next) {
 router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
 });
-
+  
+router.get('/:nick', function(req, res, next) {
+  Cat.findOne({nick:req.params.nick}, function(err, tree){
+  if(err) return next(err)
+  if(!tree) return next(new Error("Нет такого котенка в этом мультике"))
+  res.render('tree', {
+  title: tree.title,
+  picture: tree.avatar,
+  desc: tree.desc
+  })
+  })
+  })
 module.exports = router;
