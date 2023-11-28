30c3a4d2b2ab469f7c1c7588ca6cf31089bc2839
kitron4ik <craftukww@gmail.com>
2023-11-28 21:42:47 +0300

    Сохранение сессии в mongoDB

--- forest/app.js
+++ forest/app.js
@@ -25,13 +25,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var MongoStore = require('connect-mongo');
 app.use(session({
-  secret: "ThreeCats",
+  secret: "Forest",
   cookie:{maxAge:60*1000},
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/forest'})
   }))
-    
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/trees', treesRouter);
--- forest/routes/index.js
+++ forest/routes/index.js
@@ -6,14 +6,8 @@ const Tree = require("../models/tree").Tree;
 router.get('/', async (req, res, next) => {
   try {
     const menu = await Tree.find({}, { _id: 0, title: 1, nick: 1 });
-    res.cookie('greeting', 'Hi!!!').render('index', { 
-      title:'Express', 
-      menu:menu 
-    });
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
+    req.session.greeting = "Hi!!!"
+    res.render('index', { title: 'Express', menu:menu });
   } catch (err) {
     next(err);
   }
