b84de23acccd0e792ae2619a03c7912fcf4d7315
kitron4ik <craftukww@gmail.com>
2023-11-29 16:17:42 +0300

    Настроил логику пользователя

--- forest/routes/index.js
+++ forest/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const Tree = require("../models/tree").Tree;
+var User = require("./../models/User").User
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-   
     req.session.greeting = "Hi!!!"
     res.render('index', { title: 'Express', counter:req.session.counter });
   } catch (err) {
@@ -13,15 +14,37 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+
+
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
 
-router.post('/logreg', function(req, res, next) {
-  //var username = req.body.username
-  //var password = req.body.password  
-});
   
+    
+  router.post('/logreg', function(req, res, next) {
+      var username = req.body.username
+      var password = req.body.password
+      User.findOne({username:username},function(err,user){
+      if(err) return next(err)
+      if(user){
+      if(user.checkPassword(password)){
+      req.session.user = user._id
+      res.redirect('/')
+      } else {
+      res.render('logreg', {title: 'Вход'})
+      }
+      } else {
+      var user = new User({username:username,password:password})
+      user.save(function(err,user){
+      if(err) return next(err)
+      req.session.user = user._id
+      res.redirect('/')
+    })
+    }
+  })
+});
+    
 
-
+  
 module.exports = router;
