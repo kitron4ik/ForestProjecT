6fbe0314995645fa1f4f3c3fdb9507da576fde70
kitron4ik <craftukww@gmail.com>
2023-11-29 16:38:00 +0300

    Создал глобальную переменную user

--- forest/app.js
+++ forest/app.js
@@ -43,6 +43,7 @@ app.use(session({
 
 
 app.use(require("./middleware/createMenu.js"));
+app.use(require("./middleware/createUser.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/trees', treesRouter);
