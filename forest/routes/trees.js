2d7bbdee74458a839b0edf8e2ef14111a7e3d523
kitron4ik <craftukww@gmail.com>
2023-12-02 17:15:04 +0300

    Сделал вывод персонажей на экран из базы данных MySQL

--- forest/routes/trees.js
+++ forest/routes/trees.js
@@ -1,28 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const Tree = require("../models/tree").Tree;
+//const Tree = require("../models/tree").Tree;
 const async = require("async")
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var checkAuth = require("./../middleware/checkAuth.js")
 
-router.get('/:nick',checkAuth, async function(req, res, next) {
-  try {
-    const [tree, trees] = await Promise.all([
-      Tree.findOne({ nick: req.params.nick })
-    ]);
-    if (!tree) {
-      throw new Error("Нет такого");
-    }
-    renderTree(res, tree.title, tree.avatar, tree.desc, trees);
-  } catch (err) {
-    next(err);
-  }
-});
-function renderTree(res, title, picture, desc, trees) {
-  res.render('tree', {
-    title: title,
-    picture: picture,
-    desc: desc,
-  });
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM trees WHERE trees.nick = '${req.params.nick}'`, (err,trees) => {
+  if(err) {
+    console.log(err);
+    if(err) return next(err)
+    } else {
+      if(trees.length == 0) return next(new Error("Такого пышного древа нет"))
+        var tree = trees[0];
+        res.render('tree', {
+          title: tree.title,
+          picture: tree.avatar,
+          desc: tree.about
+  })
 }
+})
+
+});
+  
+
+
+
+// router.get('/:nick', async function(req, res, next) {
+//   try {
+//     const [tree, trees] = await Promise.all([
+//       Tree.findOne({ nick: req.params.nick })
+//     ]);
+//     if (!tree) {
+//       throw new Error("Нет такого");
+//     }
+//     renderTree(res, tree.title, tree.avatar, tree.desc, trees);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// function renderTree(res, title, picture, desc, trees) {
+//   res.render('tree', {
+//     title: title,
+//     picture: picture,
+//     desc: desc,
+//   });
+// }
 
 module.exports = router;
