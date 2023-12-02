8e7104f8cee6e72917d8faa53e3e8d4c42a6cd38
kitron4ik <craftukww@gmail.com>
2023-12-02 18:40:08 +0300

    Вернул сохранение информации о залогиненом пользователе при помощи MySQL

--- forest/middleware/createUser.js
+++ forest/middleware/createUser.js
@@ -1,17 +1,17 @@
-const User = require("../models/user").User;
+//const User = require("../models/user").User;
 
-module.exports = async function(req, res, next) {
-  res.locals.user = null
-  // try {
-  //   const user = await User.findById(req.session.user).exec();
-  //   if (!user) {
-  //     res.locals.user = null;
-  //   } else {
-  //     res.locals.user = user;
-  //   }
-  //   next();
-  // } catch (err) {
-  //   next(err);
-  // }
-  next();
-};
+var db = require('../mySQLConnect.js');
+module.exports = function(req,res,next) {
+res.locals.user = null
+console.log('Проверяем сессию')
+console.log(req.session.user)
+db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+users) => {
+if(err)
+return next(err)
+if(users.length > 0) {
+res.locals.user = users[0];
+}
+next()
+})
+}
