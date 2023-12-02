ba4b6747e781d462efb7e1040d0d56b7b4272358
kitron4ik <craftukww@gmail.com>
2023-12-02 17:19:12 +0300

    Сделал заполнение меню из базы

--- forest/middleware/createMenu.js
+++ forest/middleware/createMenu.js
@@ -3,7 +3,7 @@ const Tree = require("./../models/tree").Tree;
 module.exports = async function(req, res, next) {
   try {
     res.locals.nav = [];
-    const result = await Tree.find({}, { _id: 0, title: 1, nick: 1 });
+    const result = await Tree.find(null, { _id: 0, title: 1, nick: 1 });
     res.locals.nav = result;
     next();
   } catch (err) {
