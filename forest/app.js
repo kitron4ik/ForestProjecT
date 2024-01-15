8ec411f90253b45fcfe89ae7c989396d397c710c
kitron4ik <craftukww@gmail.com>
2024-01-15 12:20:24 +0300

    .

--- forest/app.js
+++ forest/app.js
@@ -18,7 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('ejs',require('ejs-locals'));
+app.engine('ejs', require('ejs-locals'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,16 +30,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 var MongoStore = require('connect-mongo');
 app.use(session({
   secret: "Forest",
-  cookie:{maxAge:60*1000},
+  cookie: { maxAge: 60 * 1000 },
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({mongoUrl: 'mongodb://localhost/forest'})
-  }))
-  app.use(function(req,res,next){
-    req.session.counter = req.session.counter +1 || 1
-    next()
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/forest' })
+}))
+app.use(function (req, res, next) {
+  req.session.counter = req.session.counter + 1 || 1
+  next()
 })
-   
+
 
 
 app.use(require("./middleware/createMenu.js"));
@@ -50,19 +50,19 @@ app.use('/trees', treesRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: "Дерево потерялось" });
 });
 
 module.exports = app;
