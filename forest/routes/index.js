4868ad6330b848bcfce69ed68e33d06142901b4e
kitron4ik <craftukww@gmail.com>
2024-01-15 12:16:35 +0300

    .

--- forest/routes/index.js
+++ forest/routes/index.js
@@ -8,7 +8,7 @@ var User = require("./../models/User").User
 router.get('/', async (req, res, next) => {
   try {
     req.session.greeting = "Hi!!!"
-    res.render('index', { title: 'Express', counter:req.session.counter });
+    res.render('index', { title: 'Express', counter: req.session.counter });
   } catch (err) {
     next(err);
   }
@@ -16,35 +16,36 @@ router.get('/', async (req, res, next) => {
 
 
 
-router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
-  });
-
-  
-    
-  router.post('/logreg', function(req, res, next) {
-      var username = req.body.username
-      var password = req.body.password
-      User.findOne({username:username},function(err,user){
-      if(err) return next(err)
-      if(user){
-      if(user.checkPassword(password)){
-      req.session.user = user._id
-      res.redirect('/')
-      } else {
-      res.render('logreg', {title: 'Вход'})
-      }
-      } else {
-      var user = new User({username:username,password:password})
-      user.save(function(err,user){
-      if(err) return next(err)
-      req.session.user = user._id
-      res.redirect('/')
-    })
-    }
-  })
+router.get('/logreg', function (req, res, next) {
+  res.render('logreg', { title: 'Вход' });
 });
-    
 
-  
+
+
+router.post('/logreg', async function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const user = await User.findOne({ username });
+
+    if (user) {
+      if (user.checkPassword(password)) {
+        req.session.user = user._id;
+        res.redirect('/');
+      } else {
+        res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
+      }
+    } else {
+      const newUser = new User({ username, password });
+      await newUser.save();
+      req.session.user = newUser._id;
+      res.redirect('/');
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
 module.exports = router;
